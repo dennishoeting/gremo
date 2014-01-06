3c198d6537d445a2da148ede875af5ead911ddbd
dennishoeting <dennis.hoeting@gmail.com>
2014-01-06 20:17:29 +0100

    removed all

--- GreMo App/src/com/GreMo/GreMoApp/model/GremoSensor.java
+++
@@ -1,9 +0,0 @@
-package com.GreMo.GreMoApp.model;
-
-/**
- * @author DennisHoeting
- * @version 1.0, 25.06.13
- */
-abstract class GremoSensor extends GremoDevice {
-    public abstract void senseOnce();
-}
--- MobileSensorApp/src/com/gremo/mobilesensorapp/model/Sensor.java
+++
@@ -1,8 +0,0 @@
-package com.gremo.mobilesensorapp.model;
-
-/**
- * Created by DennisHoeting on 25.06.13.
- */
-public abstract class Sensor extends Device {
-    public abstract void senseOnce();
-}
